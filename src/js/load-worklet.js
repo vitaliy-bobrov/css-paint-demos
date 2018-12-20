0528081105b36cbe84a89c4ae2f393b7bc0ae524
Vitaliy Bobrov <bobrov1989@gmail.com>
2018-12-20 23:56:47 +0100

    Update load script

--- src/js/load-worklet.js
+++ src/js/load-worklet.js
@@ -1,18 +1,18 @@
 if ('paintWorklet' in CSS) {
   // Safari TP fix.
   // Now Safari only accepts source code as a string instead file path.
-  if ('safari' in window) {
-    fetch('paint.js')
-      .then(async (res) => {
-        const blob = await res.blob();
-        const reader = new FileReader();
+  if (navigator.userAgent.includes('Safari') && !navigator.userAgent.includes('Chrome')) {
+    (async function() {
+      const response = await fetch('paint.js');
+      const blob = await response.blob();
+      const reader = new FileReader();
 
-        reader.addEventListener('load', () => {
-          CSS.paintWorklet.addModule(reader.result);
-        });
-
-        reader.readAsText(blob);
+      reader.addEventListener('load', () => {
+        CSS.paintWorklet.addModule(reader.result);
       });
+
+      reader.readAsText(blob);
+    })();
   } else {
     CSS.paintWorklet.addModule('paint.js');
   }
